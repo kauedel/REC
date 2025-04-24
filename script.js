32242db30a465f42b891ea9fe370ef99fe8012a3
KAUÊ MACHADO DE LIMA <kaue_m_lima@estudante.sesisenai.org.br>
2025-04-24 07:53:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+class Personagem {
+    constructor(nome, vida = 100, forca = 200, poder = 70) {
+      this.nome = nome;
+      this.vida = vida;
+      this.forca = forca;
+      this.poder = poder;
+    }
+  }
