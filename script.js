035fde03e67dd0f7cbdf228c8bf0a0446d8b7b73
KAUÊ MACHADO DE LIMA <kaue_m_lima@estudante.sesisenai.org.br>
2025-04-24 08:07:56 -0300

    Criação das classes Macaco, Russo e Cartoleiro herdando de Personagem

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
+
 class Personagem {
-    constructor(nome, vida = 100, forca = 200, poder = 70) {
-      this.nome = nome;
-      this.vida = vida;
-      this.forca = forca;
-      this.poder = poder;
+    constructor(nome) {
+      this.nome = nome;  
+      this.vida = 100;   
+      this.forca = 10;   
+      this.poder = 20;   
+    }
+  
+    
+    atacar() {
+      return `${this.nome} ataca com força ${this.forca}!`;
     }
   }
+  
+  
+  class MacacoMago extends Personagem {
+    constructor(nome) {
+      super(nome); 
+      this.poder = 40;   
+      this.vida = 80;    
+    }
+  
+    
+    lancarMagia() {
+      return `${this.nome} lança magia com poder ${this.poder}`;
+    }
+  }
+  
+  
+  class RussoBruxo extends Personagem {
+    constructor(nome) {
+      super(nome);
+      this.vida = 150;  
+      this.forca = 25;  
+    }
+  
+    
+    beberVodka() {
+      this.vida += 10;  
+      return `${this.nome} bebe vodka e recupera vida Vida agora: ${this.vida}`;
+    }
+  }
+  
+  
+  class Cartoleiro extends Personagem {
+    constructor(nome) {
+      super(nome);
+      }
+  
+    
+    preverFuturo() {
+      return `${this.nome} diz: Você terá sorte hoje`;
+    }
+  }
+  
+  
+  const macaco = new MacacoMago("King Kong");
+  const russo = new RussoBruxo("Boris");
+  const cartoleiro = new Cartoleiro("Zé");
+  
+  
+  console.log(macaco.atacar());
+  console.log(macaco.lancarMagia());
+  
+  console.log(russo.atacar());
+  console.log(russo.beberVodka());
+  
+  console.log(cartoleiro.atacar());
+  console.log(cartoleiro.preverFuturo());
